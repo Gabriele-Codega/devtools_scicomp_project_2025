906a64d1556afc5513848ca176d46ff4bf830e54
Gabriele Codega <gcodega@sissa.it>
2025-03-25 14:26:38 +0100

    - docker GitHub workflow - CUDA checks - readme - requirements

--- src/matmul/__init__.py
+++ src/matmul/__init__.py
@@ -1,11 +1,17 @@
+import warnings
+
 __all__ = [
         'matmul',
         'matmul_numba_serial',
         'matmul_numba_cpu',
-        'matmul_numba_gpu',
         'matmul_numba_block_serial',
-        'matmul_numba_block_cpu',
-        'matmul_numba_block_gpu']
-
-
-from .routines import matmul, matmul_numba_serial, matmul_numba_cpu, matmul_numba_gpu, matmul_numba_block_serial, matmul_numba_block_cpu, matmul_numba_block_gpu
+        'matmul_numba_block_cpu']
+from .utils import custom_warning
+from .routines import matmul, matmul_numba_serial, matmul_numba_cpu, matmul_numba_block_serial, matmul_numba_block_cpu 
+try:
+    from .routines import matmul_numba_gpu, matmul_numba_block_gpu
+    __all__.append('matmul_numba_gpu')
+    __all__.append('matmul_numba_block_gpu')
+except ImportError:
+    warnings.formatwarning = custom_warning
+    warnings.warn("CUDA not found: GPU functions won't be available.")
--- src/matmul/routines.py
+++ src/matmul/routines.py
@@ -66,51 +66,52 @@ def matmul_numba_block_serial(A,B,C, bs=64):
                         for j in range(jj,jmax):
                             C[i,j] += A[i,k] * B[k,j]
 
-@cuda.jit(void(float64[:,::1],float64[:,::1],float64[:,:]), cache=True, debug=False)
-def matmul_numba_gpu(A,B,C):
-    # this only has effect if function is compiled with debug = True
-    assert (A.shape[0] == C.shape[0]) and (A.shape[1] == B.shape[0]) and (B.shape[1] == C.shape[1]), "Matrices have incompatible shapes"
-    i, j = cuda.grid(ndim=2)
-    if i < C.shape[0] and j < C.shape[1]:
+if cuda.is_available():
+    @cuda.jit(void(float64[:,::1],float64[:,::1],float64[:,:]), cache=True, debug=False)
+    def matmul_numba_gpu(A,B,C):
+        # this only has effect if function is compiled with debug = True
+        assert (A.shape[0] == C.shape[0]) and (A.shape[1] == B.shape[0]) and (B.shape[1] == C.shape[1]), "Matrices have incompatible shapes"
+        i, j = cuda.grid(ndim=2)
+        if i < C.shape[0] and j < C.shape[1]:
+            tmp = 0.
+            for k in range(B.shape[0]):
+                tmp += A[i,k] * B[k,j]
+            C[i,j] = tmp
+
+    BLOCK_SIZE = 16
+    @cuda.jit(void(float64[:,::1],float64[:,::1],float64[:,:]), cache=True, debug=False)
+    def matmul_numba_block_gpu(A,B,C):
+        # this only has effect if function is compiled with debug = True
+        assert (A.shape[0] == C.shape[0]) and (A.shape[1] == B.shape[0]) and (B.shape[1] == C.shape[1]), "Matrices have incompatible shapes"
+
+        bi = cuda.blockIdx.y
+        bj = cuda.blockIdx.x
+        ti = cuda.threadIdx.y
+        tj = cuda.threadIdx.x
+        bh = cuda.blockDim.y
+        bw = cuda.blockDim.x
+        gi = bi*bh + ti
+        gj = bj*bw + tj
+        nblocks = (A.shape[1] + BLOCK_SIZE - 1)//BLOCK_SIZE
+
+        Ashared = cuda.shared.array(shape=(BLOCK_SIZE,BLOCK_SIZE),dtype=float64)
+        Bshared = cuda.shared.array(shape=(BLOCK_SIZE,BLOCK_SIZE),dtype=float64)
         tmp = 0.
-        for k in range(B.shape[0]):
-            tmp += A[i,k] * B[k,j]
-        C[i,j] = tmp
+        for b in range(nblocks):
 
-BLOCK_SIZE = 16
-@cuda.jit(void(float64[:,::1],float64[:,::1],float64[:,:]), cache=True, debug=False)
-def matmul_numba_block_gpu(A,B,C):
-    # this only has effect if function is compiled with debug = True
-    assert (A.shape[0] == C.shape[0]) and (A.shape[1] == B.shape[0]) and (B.shape[1] == C.shape[1]), "Matrices have incompatible shapes"
+            Ashared[ti,tj] = 0
+            Bshared[ti,tj] = 0
+            
+            if gi < A.shape[0] and (tj + b*BLOCK_SIZE) < A.shape[1]:
+                Ashared[ti,tj] = A[gi,tj + b*BLOCK_SIZE]
+            if (ti + b*BLOCK_SIZE) < B.shape[0] and gj < B.shape[1]:
+                Bshared[ti,tj] = B[ti + b*BLOCK_SIZE,gj]
+            cuda.syncthreads()
 
-    bi = cuda.blockIdx.y
-    bj = cuda.blockIdx.x
-    ti = cuda.threadIdx.y
-    tj = cuda.threadIdx.x
-    bh = cuda.blockDim.y
-    bw = cuda.blockDim.x
-    gi = bi*bh + ti
-    gj = bj*bw + tj
-    nblocks = (A.shape[1] + BLOCK_SIZE - 1)//BLOCK_SIZE
+            for k in range(BLOCK_SIZE):
+                tmp += Ashared[ti,k] * Bshared[k,tj]
+            cuda.syncthreads()
 
-    Ashared = cuda.shared.array(shape=(BLOCK_SIZE,BLOCK_SIZE),dtype=float64)
-    Bshared = cuda.shared.array(shape=(BLOCK_SIZE,BLOCK_SIZE),dtype=float64)
-    tmp = 0.
-    for b in range(nblocks):
-
-        Ashared[ti,tj] = 0
-        Bshared[ti,tj] = 0
-        
-        if gi < A.shape[0] and (tj + b*BLOCK_SIZE) < A.shape[1]:
-            Ashared[ti,tj] = A[gi,tj + b*BLOCK_SIZE]
-        if (ti + b*BLOCK_SIZE) < B.shape[0] and gj < B.shape[1]:
-            Bshared[ti,tj] = B[ti + b*BLOCK_SIZE,gj]
-        cuda.syncthreads()
-
-        for k in range(BLOCK_SIZE):
-            tmp += Ashared[ti,k] * Bshared[k,tj]
-        cuda.syncthreads()
-
-    if gi < C.shape[0] and gj < C.shape[1]:
-        C[gi,gj] = tmp
+        if gi < C.shape[0] and gj < C.shape[1]:
+            C[gi,gj] = tmp
 
